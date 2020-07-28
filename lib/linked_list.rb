33ceb25dd506e62eac097795deee4d085cc4f41c
Jack Cullen <jpcullen20@gmail.com>
2020-07-28 09:33:33 -0600

    Add append count and to_string methods

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -3,4 +3,24 @@ class LinkedList
   def initialize
     @head = nil
   end
+
+  def append(surname)
+    node = Node.new(surname)
+    @head = node if !@head
+  end
+
+  def count
+    return 0 if !@head
+    count = 1
+    current_node = @head
+    while current_node.next_node
+      count += 1
+      current_node = current_node.next_node
+    end
+    count
+  end
+
+  def to_string
+    'The' + @head.surname + 'Family'
+  end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -1,4 +1,5 @@
 require './lib/linked_list.rb'
+require './lib/node.rb'
 require 'minitest/autorun'
 require 'minitest/pride'
 
@@ -13,5 +14,22 @@ class LinkedListTest < Minitest::Test
 
   def test_head_is_nil
     assert_nil @linked_list.head
+  end
+
+  def test_append
+    @linked_list.append('West')
+    assert_instance_of Node, @linked_list.head
+    assert_equal 'West', @linked_list.head.surname
+    assert_nil @linked_list.head.next_node
+  end
+
+  def test_count
+    @linked_list.append('West')
+    assert_equal 1, @linked_list.count
+  end
+
+  def test_to_string
+    @linked_list.append('West')
+    assert_equal 'The West family', @linked_list.to_string
   end
 end
